fc113b0fe9f14418d514b429ce00c94ec1dc1073
tajmirik <tajmirik@gmail.com>
2024-07-21 15:01:04 +0330

    added pin defines

--- lcd_4pin_lib.c
+++ lcd_4pin_lib.c
@@ -1,3 +1,10 @@
+#define RS(x) (x==1 ? (HAL_GPIO_WritePin(RS_GPIO_Port,RS_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(RS_GPIO_Port,RS_Pin,GPIO_PIN_RESET)))
+#define RW(x) (x==1 ? (HAL_GPIO_WritePin(RW_GPIO_Port,RW_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(RW_GPIO_Port,RW_Pin,GPIO_PIN_RESET)))
+#define EN(x) (x==1 ? (HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(E_GPIO_Port,E_Pin,GPIO_PIN_RESET)))
+#define D4(x) (x==1 ? (HAL_GPIO_WritePin(D4_GPIO_Port,D4_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(D4_GPIO_Port,D4_Pin,GPIO_PIN_RESET)))
+#define D5(x) (x==1 ? (HAL_GPIO_WritePin(D5_GPIO_Port,D5_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(D5_GPIO_Port,D5_Pin,GPIO_PIN_RESET)))
+#define D6(x) (x==1 ? (HAL_GPIO_WritePin(D6_GPIO_Port,D6_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(D6_GPIO_Port,D6_Pin,GPIO_PIN_RESET)))
+#define D7(x) (x==1 ? (HAL_GPIO_WritePin(D7_GPIO_Port,D7_Pin,GPIO_PIN_SET)): (HAL_GPIO_WritePin(D7_GPIO_Port,D7_Pin,GPIO_PIN_RESET)))
 /* USER CODE BEGIN 4 */
 void lcd_init(void)
 {
